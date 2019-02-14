c55b7b2a665acc330b0326bc98ae937ca1fc8825
JoeHooijmaijers <31613736+JoeHooijmaijers@users.noreply.github.com>
2019-02-14 21:37:03 +0100

    Initial Commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed;
+    public Vector3 offset;
+    public bool lockedOn = false;
+    public float rotationSpeed = 5.0f;
+
+
+    void LateUpdate()
+    {
+        if (!lockedOn)
+        {
+            Quaternion camTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationSpeed, Vector3.up);
+            offset = camTurnAngle * offset;
+            transform.LookAt(target);
+        }
+
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Slerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float movementSpeed;
+    public float rotationSpeed;
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);     
+        transform.rotation = Quaternion.LookRotation(movement);
+
+
+        
+    }
+ 
+}
